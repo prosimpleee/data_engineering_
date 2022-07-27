fa7df887eef1716d3903b30d14ca54501b789a6c
prosimpleee <55916170+prosimpleee@users.noreply.github.com>
2022-07-27 15:43:23 +0200

    Delete data-engineering-zoomcamp/week_1_basics_n_setup/test directory

--- data-engineering-zoomcamp/week_1_basics_n_setup/test/README.md
+++
@@ -1,9 +0,0 @@
-# How to run and build image:
-## 1. docker build:
-```python
-docker build -t script .
-```
-## 2. docker run:
-```python
-docker run script 
-```
--- data-engineering-zoomcamp/week_1_basics_n_setup/test/hello.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-update = []
-for i in range (1,5):
-    dictinary = {str(i) : i}
-    update.append(dictinary)
-df = pd.DataFrame(update)
-print(df)
