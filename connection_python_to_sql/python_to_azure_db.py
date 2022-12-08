5814f08056227fedc9dbeefd94009617c867e16e
prosimpleee <55916170+prosimpleee@users.noreply.github.com>
2022-12-08 22:56:45 +0100

    Added python_to_azure_db.py

---
+++ connection_python_to_sql/python_to_azure_db.py
@@ -0,0 +1,45 @@
+from sqlalchemy import create_engine
+import psycopg2
+import pandas as pd
+
+
+POSTGRES_USER = 'LinkedinForPython'
+POSTGRES_PASS = 'hey1DBConnection'
+POSTGRES_HOST = 'for-linkedin.postgres.database.azure.com'
+POSTGRES_PORT = 5432
+POSTGRES_DB = 'postgres'
+
+
+print('Connect to DB')
+conn = psycopg2.connect(user=POSTGRES_USER, database=POSTGRES_DB, password=POSTGRES_PASS, host=POSTGRES_HOST, port=POSTGRES_PORT)
+if conn.cursor():
+    cur = conn.cursor()
+    print('psycopg2 connected')
+engine = create_engine(f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASS}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}')
+print('sqlalchemy connected')
+
+sql_drop = '''
+DROP TABLE IF EXISTS Linkedin;
+'''
+cur.execute(sql_drop)
+conn.commit()
+
+sql = '''
+CREATE TABLE IF NOT EXISTS Linkedin (
+    id int primary key,
+    name varchar(50)
+);
+'''
+
+cur.execute(sql)
+conn.commit()
+
+sql_insert = f"INSERT INTO Linkedin VALUES (1, 'Dmitry');"
+cur.execute(sql_insert)
+conn.commit()
+
+
+sql_select = f"SELECT * FROM Linkedin;"
+routing_df = pd.read_sql(sql_select, engine)
+df = pd.DataFrame(routing_df)
+print(df)
