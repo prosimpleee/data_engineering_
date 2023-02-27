10094d428776359760bbfe65ac76d5be1fb9b249
prosimpleee <55916170+prosimpleee@users.noreply.github.com>
2023-02-27 14:00:43 +0100

    added most_profitable_companies(ss).sql

---
+++ SQL_exercises/most_profitable_companies(ss).sql
@@ -0,0 +1,28 @@
+-- Most Profitable Companies
+-- Find the 3 most profitable companies in the entire world.
+-- Output the result along with the corresponding company name.
+-- Sort the result based on profits in descending order.
+-- https://platform.stratascratch.com/coding/10354-most-profitable-companies
+
+# forbes_global_2010_2014
+company: varchar
+sector: varchar
+industry: varchar
+continent: varchar
+country: varchar
+marketvalue: float
+sales: float
+profits: float
+assets: float
+rank: int
+forbeswebpage: varchar
+
+with cte as (
+select company, 
+	   profits,
+	   rank() over (order by profits desc) as rnk
+from forbes_global_2010_2014)
+
+select company, profits
+from cte 
+where rnk < 4
