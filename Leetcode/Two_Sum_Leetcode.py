4e4a4d8618ceac7c416867528629152ea5455976
Dmitry Ryzhov <55916170+prosimpleee@users.noreply.github.com>
2023-03-02 08:40:04 +0100

    deleted repo leetcode

--- Leetcode/Two_Sum_Leetcode.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        for i in range(len(nums) - 1):
-            for k in range(i+1, len(nums)):
-                if nums[i] + nums[k] == target:
-                    return [i, k]
-
